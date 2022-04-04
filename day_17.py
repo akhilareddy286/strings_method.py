765abd8f13e7c49b1ba799299577c0e66d9905e5
akhilareddy286 <102401859+akhilareddy286@users.noreply.github.com>
2022-04-04 11:14:31 +0530

    Add files via upload

---
+++ day_17.py
@@ -0,0 +1,79 @@
+# a="akhila"
+# print(a.removeprefix('a'))
+# print(a.removeprefix('ak'))
+# print(a.removeprefix('akh'))
+# print(a.removesuffix('a'))
+# print(a.removesuffix('la'))
+# print(a.removesuffix('ila'))
+
+# a='akhila reddy'
+# print(a.isalnum())
+
+# a='akhilareddy'
+# print(a.isalnum())
+
+# a='akhilareddy31'
+# print(a.isalnum())
+
+# a='akhila reddy'
+# print(a.isalpha())
+
+# a='akhilareddy'
+# print(a.isalpha())
+
+# a='akhila456'
+# print(a.isalpha())
+
+# a='r'
+# print(a.isalpha())
+
+# a=chr(0)
+# b=chr(32)
+# print(a.isspace())
+# print(b.isspace())
+# print("akhila"+" "+"reddy")
+# print("akhila"+chr(32)+"reddy")
+# print("akhila","reddy",sep=" ")
+
+# print(chr(32)+"hello")
+# print(chr(0)+"hello")
+
+# print(ord('a'))
+# print(ord('z'))
+# print(ord('h'))
+# print(ord('G'))
+# print(ord('N'))
+# print(ord('R'))
+# print(ord('0'))
+# print(ord('9'))
+# print(ord('!'))
+# print(ord('\a'))
+# print(ord('\r'))
+
+# v=bin(5)
+# print(v,type(v))
+# a=0b101
+# print(a,type(a))
+# b=0B101
+# print(b,type(b))
+# a=bin(8)
+# print(a,type(a))
+# b=0b1000
+# print(b,type(b))
+
+# a=oct(13)
+# print(a,type(a))
+# b=0o15
+# print(b,type(b))
+# c=0O15
+# print(c,type(c))
+
+a=hex(20)
+print(a,type(a))
+print(0x14)
+print(0X14)
+print(0XA)
+print(0xa)
+print(0xb)
+print(0xf)
+print(0XE)
